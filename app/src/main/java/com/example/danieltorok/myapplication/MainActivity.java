2b8aedfd3fbdf6e23fdc230c5a657e98223c3b77
Daniel Torok <danieltorok@Daniels-MacBook-Pro.local>
2017-11-14 14:58:57 +0100

    navigation1-2

--- app/src/main/java/com/example/danieltorok/myapplication/MainActivity.java
+++ app/src/main/java/com/example/danieltorok/myapplication/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.danieltorok.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button navigateToAnOtherScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        navigateToAnOtherScreen = (Button) findViewById(R.id.gombid);
+
+        navigateToAnOtherScreen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view)  {
+
+                return navigacioMasodikScreenre();
+            }
+        });
     }
+
+    private void navigacioMasodikScreenre()  {
+        Intent navigaciointent = new Intent(this, MasodikActivity.class);
+        startActivity(navigaciointent);
+
+    }
+
+
+
+
 }
 
 
