d8d2131a7f5945b3d48969a6112cffc21a328132
Daniel Torok <danieltorok@Daniels-MacBook-Pro.local>
2017-11-14 15:03:45 +0100

    oops

--- app/src/main/java/com/example/danieltorok/myapplication/MainActivity.java
+++ app/src/main/java/com/example/danieltorok/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (View view)  {
 
-                return navigacioMasodikScreenre();
+                navigacioMasodikScreenre();
             }
         });
     }
